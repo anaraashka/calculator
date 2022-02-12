a71e472cd25980cb4f186c0bd031a650c1c99b44
anaraashka <99520152+anaraashka@users.noreply.github.com>
2022-02-12 14:08:10 -0600

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+
+read -p "Enter first number: " FIRST
+until [[ $FIRST =~ ^[+]?[0-9]+$ ]]
+do
+echo "Sorry integers only"
+sleep 1
+read -p "Try again, Please enter first number: " FIRST
+done
+sleep 2
+
+
+
+
+read -p "Enter one of these (+)(-)(*)(/) " SYMBOL
+
+
+
+read -p "Enter second number: " SECOND
+until [[ $SECOND =~ ^[+]?[0-9]+$ ]]
+do
+echo "Sorry integers only"
+sleep 1
+read -p "Try again, Please enter second number: " SECOND
+done
+let SUM=$FIRST$SYMBOL$SECOND
+echo "Result: $SUM "
+
+
+
+source /root/calculator.sh
